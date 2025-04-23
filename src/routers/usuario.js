a5c342b99ffaf19ea04f55e5a83e06707f921519
JVQP <jaumvit0r222@gmail.com>
2025-04-23 20:51:27 -0300

    estilizando página de cadastro

--- src/routers/usuario.js
+++ src/routers/usuario.js
@@ -7,5 +7,16 @@ router.get('/', (req, res) => {
     res.render('cadastro_usuarios');
 });
 
+router.post('/cadastro', (req, res) => {
+
+let inputEmail = req.body.inputEmail;
+let nome_completo = req.body.inputNome;
+let senha = req.body.inputSenha;
+let confirmar = req.body.inputConfirmar;
+let tipo = inputTipoUsuario;
+
+
+
+});
 
 module.exports = router;
